abb2166f05831cfa82150972c23919575c2382e6
ooblog <ooblog@gmail.com>
2016-04-20 01:42:38 +0900

    以前「nico-tu」という名前だったDNS設定ツールをアイコンとか変更してomakeから独立。

---
+++ DNS8888.py
@@ -0,0 +1,167 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division,print_function,absolute_import,unicode_literals
+import sys
+import os
+import re
+os.chdir(sys.path[0])
+sys.path.append("LTsv")
+from LTsv_printf import *
+from LTsv_file   import *
+#from LTsv_time   import *
+#from LTsv_calc   import *
+#from LTsv_joy    import *
+#from LTsv_kbd    import *
+from LTsv_gui    import *
+
+DNS8888_T,DNS8888_W,DNS8888_H,DNS8888_I="DNS8888",640,380,None
+DNS8888_congigpath,DNS8888_hostspath,DNS8888_resolvpath,DNS8888_temppath="DNS8888.tsv","/etc/hosts","/etc/resolv.conf","/tmp/"
+DNS8888_ltsv,DNS8888_config,DNS8888_dnslist="","",""
+DNS8888_fontname,DNS8888_fontsize="kantray5x5comic",12; DNS8888_font="{0},{1}".format(DNS8888_fontname,DNS8888_fontsize)
+DNS8888_txtedit,DNS8888_filer,DNS8888_player="leafpad","rox","gnomemplayershell"
+DNS8888_capitle,DNS8888_capwait,DNS8888_rewrite=".*",10000," - Mozilla Firefox"
+
+def DNS8888_dnsadd_kernel(window_objvoid=None,window_objptr=None):
+    DNS8888_resolvlist=LTsv_loadfile(DNS8888_resolvpath)
+    DNS8888_resolvlist_splits=DNS8888_resolvlist.rstrip('\n').split('\n') if len(DNS8888_resolvlist) > 0 else []
+    for dns in DNS8888_dnslist.rstrip('\n').split('\n'):
+        if "nameserver " in LTsv_pickdatanum(dns,1):
+            DNS8888_resolvlist_splits.append(LTsv_pickdatanum(dns,1))
+    DNS8888_resolvlist_splits=sorted(set(DNS8888_resolvlist_splits),key=DNS8888_resolvlist_splits.index)
+    DNS8888_resolvlist="".join("{0}\n".format(dns) for dns in DNS8888_resolvlist_splits)
+    LTsv_saveplain(DNS8888_resolvpath,DNS8888_resolvlist)
+    DNS8888_dnsexist_kernel()
+
+def DNS8888_dnsexist_kernel():
+    DNS8888_resolvlist=LTsv_loadfile(DNS8888_resolvpath)
+    DNS8888_dns_splits=DNS8888_dnslist.rstrip('\n').split('\n'); DNS8888_dns_splitslen=len(DNS8888_dns_splits)
+    for dns in DNS8888_dns_splits:
+       if "nameserver " in dns:
+           DNS8888_dns_splitslen=DNS8888_dns_splitslen-1 if dns[dns.find("nameserver ")+len("nameserver "):] in DNS8888_resolvlist else DNS8888_dns_splitslen
+    LTsv_widget_disableenable(DNS8888_button_dns,True if DNS8888_dns_splitslen > 0 else False)
+
+def DNS8888_grep_kernel():
+    DNS8888_combobox_number=LTsv_widget_getnumber(DNS8888_combobox_flash)
+    DNS8888_greplist=[]
+    DNS8888_lsof_input="lsof -n | grep Flash"
+    DNS8888_lsof_output=LTsv_subprocess(DNS8888_lsof_input,LTsv_subprocess_shell=True).rstrip('\n')
+    DNS8888_lsof_splits=DNS8888_lsof_output.rstrip('\n').split('\n') if len(DNS8888_lsof_output) > 0 else []
+    for LTsv_lsof in DNS8888_lsof_splits:
+        DNS8888_lsproc_input="ls -l /proc/{0}/fd | grep Flash".format(LTsv_pickdatanum(LTsv_lsof,0))
+        DNS8888_lsproc_output=LTsv_subprocess(DNS8888_lsproc_input,LTsv_subprocess_shell=True)
+        DNS8888_lsproc_splits=DNS8888_lsproc_output.rstrip('\n').split('\n') if len(DNS8888_lsproc_output) > 0 else []
+        for  LTsv_lsproc in DNS8888_lsproc_splits:
+            LTsv_fdproc=LTsv_lsproc[0:LTsv_lsproc.find(" ->")].rsplit(' ')[-1]
+            DNS8888_greplist.append("/proc/{0}/fd/{1}\n".format(LTsv_lsof.split('\t')[0],LTsv_fdproc))
+    DNS8888_greplist=sorted(set(DNS8888_greplist),key=DNS8888_greplist.index)
+    LTsv_combobox_list(DNS8888_combobox_flash,"".join(DNS8888_greplist))
+    LTsv_widget_setnumber(DNS8888_combobox_flash,DNS8888_combobox_number)
+    LTsv_widget_disableenable(DNS8888_button_play,True if len(LTsv_widget_gettext(DNS8888_combobox_flash)) > 0 else False)
+
+def DNS8888_play_kernel(window_objvoid=None,window_objptr=None):
+    if len(LTsv_widget_gettext(DNS8888_combobox_flash)) > 0:
+        LTsv_subprocess("{0} {1}".format(DNS8888_player,LTsv_widget_gettext(DNS8888_combobox_flash)))
+        DNS8888_dnsexist_kernel()
+    return 0
+
+def DNS8888_open_kernel(window_objvoid=None,window_objptr=None):
+    if len(LTsv_widget_gettext(DNS8888_combobox_flash)) > 0:
+        LTsv_nicotu_open_output=LTsv_subprocess("cp {0} /tmp/debug{1}.mp4".format(LTsv_widget_gettext(DNS8888_combobox_flash),LTsv_widget_getnumber(DNS8888_combobox_flash)))
+    LTsv_nicotu_filer_output=LTsv_subprocess("{0} {1}".format(DNS8888_filer,DNS8888_temppath))
+    DNS8888_dnsexist_kernel()
+
+def DNS8888_edit_shell(editerpath,filepath):
+    def DNS8888_edit_kernel(callback_void=None,callback_ptr=None):
+        LTsv_subprocess("{0} {1}".format(editerpath,filepath))
+        DNS8888_configload(DNS8888_congigpath)
+        DNS8888_dnsexist_kernel()
+    return DNS8888_edit_kernel
+
+def DNS8888_window_getID(window_objvoid=None,window_objptr=None):
+    global DNS8888_T,DNS8888_W,DNS8888_H,DNS8888_I
+    if LTsv_window_title(LTsv_window_foreground()) == DNS8888_T:
+        DNS8888_I="{0}".format(LTsv_window_foreground())
+        LTsv_widget_settext(DNS8888_entry_capitle,"{0}:{1}".format(DNS8888_T,DNS8888_I))
+        LTsv_window_after(DNS8888_window,event_b=DNS8888_window_gettitle,event_i="DNS8888_window_gettitle",event_w=DNS8888_capwait)
+    else:
+        LTsv_window_after(DNS8888_window,event_b=DNS8888_window_getID,event_i="DNS8888_window_getID",event_w=100)
+
+def DNS8888_window_gettitle(window_objvoid=None,window_objptr=None):
+    if LTsv_window_foreground() != DNS8888_I:
+        DNS8888_gettitle=LTsv_window_title(LTsv_window_foreground())
+        try:
+            rewrite_research=re.search(re.compile(DNS8888_capitle),DNS8888_gettitle)
+        except re.error:
+            None
+        else:
+            if rewrite_research:
+                rewrite_case_first=LTsv_readlinefirsts(DNS8888_rewrite)
+                for rewrite in DNS8888_rewrite.rstrip('\n').split('\n'):
+                    rewrite_first=LTsv_readlinefirsts(rewrite); rewrite_rest=LTsv_readlinerest(rewrite,rewrite_first)
+                    DNS8888_gettitle=re.sub(re.compile(rewrite_first),rewrite_rest,DNS8888_gettitle)
+                LTsv_widget_settext(DNS8888_entry_capitle,DNS8888_gettitle)
+    LTsv_window_after(DNS8888_window,event_b=DNS8888_window_gettitle,event_i="DNS8888_window_gettitle",event_w=DNS8888_capwait)
+
+def DNS8888_configload(congigfile=DNS8888_congigpath):
+    global DNS8888_congigpath,DNS8888_hostspath,DNS8888_resolvpath,DNS8888_temppath
+    global DNS8888_ltsv,DNS8888_config,DNS8888_dnslist
+    global DNS8888_fontname,DNS8888_fontsize
+    global DNS8888_txtedit,DNS8888_filer,DNS8888_player
+    global DNS8888_capitle,DNS8888_capwait,DNS8888_rewrite
+    DNS8888_ltsv=LTsv_loadfile(congigfile)
+    DNS8888_config=LTsv_getpage(DNS8888_ltsv,"DNS8888")
+    DNS8888_fontname=LTsv_readlinerest(DNS8888_config,"fontname",DNS8888_fontname)
+    DNS8888_fontsize=min(max(LTsv_intstr0x(LTsv_readlinerest(DNS8888_config,"fontsize")),5),20)
+    DNS8888_font="{0},{1}".format(DNS8888_fontname,DNS8888_fontsize)
+    if sys.platform.startswith("win"):
+        DNS8888_txtedit=LTsv_readlinerest(DNS8888_config,"txteditW","notepad")
+        DNS8888_filer=LTsv_readlinerest(DNS8888_config,"filerW","start")
+        DNS8888_player=LTsv_readlinerest(DNS8888_config,"playerW","mplayer2")
+        DNS8888_hostspath=LTsv_readlinerest(DNS8888_config,"hostsW",DNS8888_hostspath)
+        DNS8888_temppath=LTsv_readlinerest(DNS8888_config,"tempW","C:\\Documents and Settings\\%username%\\Local Settings\\Temp")
+    if sys.platform.startswith("linux"):
+        DNS8888_txtedit=LTsv_readlinerest(DNS8888_config,"txteditL",DNS8888_txtedit)
+        DNS8888_filer=LTsv_readlinerest(DNS8888_config,"filerL",DNS8888_filer)
+        DNS8888_player=LTsv_readlinerest(DNS8888_config,"playerL",DNS8888_player)
+        DNS8888_hostspath=LTsv_readlinerest(DNS8888_config,"hostsL",DNS8888_hostspath)
+        DNS8888_temppath=LTsv_readlinerest(DNS8888_config,"tempL",DNS8888_temppath)
+    DNS8888_resolvpath=LTsv_readlinerest(DNS8888_config,"DNSresolv",DNS8888_resolvpath)
+    DNS8888_capitle=LTsv_readlinerest(DNS8888_config,"captitle",DNS8888_capitle)
+    DNS8888_capwait=min(max(int(float(LTsv_readlinerest(DNS8888_config,"capwait",str(DNS8888_capwait)))),1000),60000)
+    DNS8888_rewrite=LTsv_getpage(DNS8888_ltsv,"titlerewrite")
+    DNS8888_dnslist=LTsv_getpage(DNS8888_ltsv,"DNSlist")
+
+LTsv_GUI=LTsv_guiinit()
+if len(LTsv_GUI) > 0:
+    DNS8888_configload(DNS8888_congigpath)
+    DNS8888_buttonsize=DNS8888_fontsize*2
+    DNS8888_window=LTsv_window_new(event_b=LTsv_window_exit,widget_t="DNS8888",widget_w=DNS8888_W,widget_h=DNS8888_H)
+    DNS8888_label_hosts=LTsv_label_new(DNS8888_window,widget_t="hosts「{0}」".format(DNS8888_hostspath),widget_x=0,widget_y=DNS8888_buttonsize*0,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_hosts=LTsv_button_new(DNS8888_window,event_b=DNS8888_edit_shell(DNS8888_txtedit,DNS8888_hostspath),widget_t="{0} {1}".format(DNS8888_txtedit,DNS8888_hostspath),widget_x=0,widget_y=DNS8888_buttonsize*1,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_label_dns=LTsv_label_new(DNS8888_window,widget_t="DNS「nameserver 8.8.8.8」>「{0}」".format(DNS8888_resolvpath),widget_x=0,widget_y=DNS8888_buttonsize*2,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_dns=LTsv_button_new(DNS8888_window,event_b=DNS8888_dnsadd_kernel,widget_t="8.8.8.8",widget_x=DNS8888_W*0//4,widget_y=DNS8888_buttonsize*3,widget_w=DNS8888_W*1//4,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_resolv=LTsv_button_new(DNS8888_window,event_b=DNS8888_edit_shell(DNS8888_txtedit,DNS8888_resolvpath),widget_t="{0} {1}".format(DNS8888_txtedit,DNS8888_resolvpath),widget_x=DNS8888_W*1//4,widget_y=DNS8888_buttonsize*3,widget_w=DNS8888_W*3//4,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_config=LTsv_button_new(DNS8888_window,event_b=DNS8888_edit_shell(DNS8888_txtedit,DNS8888_congigpath),widget_t="{0} {1}".format(DNS8888_txtedit,DNS8888_congigpath),widget_x=0,widget_y=DNS8888_H-DNS8888_buttonsize*1,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize*1,widget_f=DNS8888_font)
+    DNS8888_label_flash=LTsv_label_new(DNS8888_window,widget_t="flash(「lsof -n | grep Flash」>「/tmp/debug0.mp4」",widget_x=0,widget_y=DNS8888_buttonsize*4,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_open=LTsv_button_new(DNS8888_window,event_b=DNS8888_open_kernel,widget_t="{0} {1}".format(DNS8888_filer,DNS8888_temppath),widget_x=DNS8888_W*0//5,widget_y=DNS8888_buttonsize*5,widget_w=DNS8888_W*1//5,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_combobox_flash=LTsv_combobox_new(DNS8888_window,event_b=None,widget_x=DNS8888_W*1//5,widget_y=DNS8888_buttonsize*5,widget_w=DNS8888_W*2//5,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_button_play=LTsv_button_new(DNS8888_window,event_b=DNS8888_play_kernel,widget_t=DNS8888_player,widget_x=DNS8888_W*3//5,widget_y=DNS8888_buttonsize*5,widget_w=DNS8888_W*2//5,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_label_capitle=LTsv_label_new(DNS8888_window,widget_t="「{0}」".format(DNS8888_capitle),widget_x=0,widget_y=DNS8888_buttonsize*6,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    DNS8888_entry_capitle=LTsv_entry_new(DNS8888_window,event_b=None,widget_t="",widget_x=0,widget_y=DNS8888_buttonsize*7,widget_w=DNS8888_W,widget_h=DNS8888_buttonsize,widget_f=DNS8888_font)
+    if sys.platform.startswith("win"):
+        LTsv_widget_disableenable(DNS8888_button_dns,False)
+        LTsv_widget_disableenable(DNS8888_button_resolv,False)
+        LTsv_widget_disableenable(DNS8888_button_open,False)
+#        LTsv_widget_disableenable(DNS8888_combobox_flash,False)
+        LTsv_widget_disableenable(DNS8888_button_play,False)
+    LTsv_window_after(DNS8888_window,event_b=DNS8888_window_getID,event_i="DNS8888_window_getID",event_w=10)
+    LTsv_widget_showhide(DNS8888_window,True)
+    DNS8888_dnsexist_kernel()
+    if sys.platform.startswith("linux"):
+        DNS8888_grep_kernel()
+    LTsv_window_main(DNS8888_window)
+
+
+# Copyright (c) 2016 ooblog
+# License: MIT
+# https://github.com/ooblog/DNS8888/blob/master/LICENSE
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# DNS8888
-「DNS8888」はLinuxの「/etc/resolv.conf」に「Public DNS」を設定したりなどするツールのPythonによる実装です。
+# 「DNS8888」はLinuxの「/etc/resolv.conf」に「Public DNS」を設定したりなどするツールのPythonによる実装です。
+
+## DNSは取扱い注意。だけど万が一に備えて取扱う方法は知っておくべき。
+
+プロバイダーから普通に付与されてるDNSは必ず控えを確保してください。通常ははプロバイダーから付与されるDNSを使うはずです。  
+変なコピペの魔法の数字とかいうフレーズに騙されてPrivate DNSに繋がないよう気を付けてください。  
+逆に全てのPublic DNSが善意で公開されてると都合のいい解釈もしないでください。  
+更にGoogleのPublic DNS&#40;8.8.8.8&#41;ですらたまにハッキング&#40;ハイジャック&#41;されるニュースとかあるのでアンテナは張ってください。  
+DNSの変更はいわゆる自己責任なので万が一危険なサイトに釣られても謝罪賠償誠意便宜の類は致しません。
+
+
+## 操作方法。
+
+    ┏DNS8888 ━━━━━━━━━━━━━━━━━━━━━━━━┓
+    ┃                   hosts「/etc/hosts」                  ┃
+    ┃｢                 leafpad /etc/hosts                   ｣┃
+    ┃        DNS「nameserver 8.8.8.8」>「/etc/resolv.conf」  ┃
+    ┃｢  8.8.8.8   ｣｢      leafpad /etc/resolv.conf          ｣┃
+    ┃    flash(「lsof -n | grep Flash」>「/tmp/debug0.mp4」  ┃
+    ┃｢ rox /tmp/  ｣｢ gnomemplayershell ｣｢ gnomemplayershell ｣┃
+    ┃                「 - Mozilla Firefox」                  ┃
+    ┃[6.2. re — 正規表現操作 — Python 3.5.1 ドキュメント    ]┃
+    ┃                                                        ┃
+    ┃                                                        ┃
+    ┃                                                        ┃
+    ┃                                                        ┃
+    ┃｢                 leafpad DNS8888.tsv                  ｣┃
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+「8.8.8.8」ボタンを押すと「/etc/resolv.conf」に「8.8.8.8」「8.8.4.4」が追加される。  
+ダイアルアップの度に「/etc/resolv.conf」は初期化されるのでその度にDNSの確認。  
+[DNS8888.tsv](DNS8888.tsv)にはGoogle以外のDNSもコメントアウトしてるが無保証です。
+
+
+## おまけ機能。
+
+* 「/etc/hosts」の確認・編集。
+* ブラウザ上のFLVやMP4の再生を試みる実験。
+* 「Mozilla Firefox」のタイトル取得・文字列置換。
+
+
+## 動作環境。
+
+Python2.7.3&#40;PuppyLinux571JP&#41;およびPython3.4.3&#40;Wine1.7.18&#41;で動作を確認しています。  
+Windowsでは起動はすれど動作しません&#40;Linux用&#41;。  
+特にWindowsの場合市販のセキュリティソフトが「c:¥windows¥system32¥drivers¥etc¥hosts」の編集をガードするはずです。
+
+
+## ライセンス・著作権など。
+
+Copyright (c) 2016 ooblog  
+License: MIT  
+[https://github.com/ooblog/DNS8888/blob/master/LICENSE](https://github.com/ooblog/DNS8888/blob/master/LICENSE "https://github.com/ooblog/DNS8888/blob/master/LICENSE")  
